6356ca56bbfbe5555692841f63e29f293db26225
Rade <rade.pilkinton@wsu.edu>
2020-11-10 23:25:16 -0800

    building :)

--- electron/electron.js
+++ electron/electron.js
@@ -9,7 +9,7 @@ function createWindow() {
         width:800,
         height:600,
         webPreferences: {
-            nodeIntegration: false,
+            nodeIntegration: true,
             nativeWindowOpen: true,
             preload: __dirname + '/preload.js'
         }
